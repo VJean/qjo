e95f2601c981bd88d2dcd830bebd48ead422951e
VJean <jean@vintache.eu>
2022-03-27 19:30:29 +0200

    Test that Maroquinerie always gets at least one event

--- test_qjo.py
+++ test_qjo.py
@@ -1,20 +1,12 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
+from qjo.venues import Maroquinerie
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestVenues(unittest.TestCase):
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_maroquinerie(self):
+        events = Maroquinerie.get_events()
+        self.assertGreater(len(events), 0)
 
 if __name__ == '__main__':
     unittest.main()
