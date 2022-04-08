06e8c6367576a90563f33e1aebd983ec8a778535
VJean <jean@vintache.eu>
2022-04-08 21:42:17 +0200

    check for folder existence

--- tests/save.py
+++ tests/save.py
@@ -1,15 +1,19 @@
+import os, sys
 from datetime import date
-
 from qjo.venues import venues
 
-# TODO check mtime different than today?
+folder_path = "./tests/agendas/"
+
+# test folder existence
+if not os.path.isdir(folder_path):
+    print("Missing test files folder, please run 'make test'")
+    sys.exit(1)
 
 for venue in venues:
     venue_name = venue.__name__.lower()
     file_path = f"./tests/agendas/{venue_name}.html"
-    try:
-        with open(file_path, "x") as f:
-            print(f"Downloading {venue_name}'s agenda at {file_path}")
-            f.write(str(venue._get_agenda_html()))
-    except FileExistsError:
-        print(f"Already downloaded {venue_name}'s agenda")
+
+    # TODO check mtime different than today?
+    with open(file_path, "w") as f:
+        print(f"Downloading {venue_name}'s agenda at {file_path}")
+        f.write(str(venue._get_agenda_html()))
